d7d3d0f43de487d84a576936ac8f0c4eb7b279f0
dplocki <dawid.plocki@gmail.com>
2025-10-18 22:00:11 +0200

    feat(database): add automatic run the migrations on script start

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
+from yoyo import get_backend, read_migrations
 from config import load_config
 from steam import SteamGamesApi
 
 
-if __name__ == "__main__":
-    config = load_config().config
-    result = SteamGamesApi(config['steam_api_key'], config['steam_id'])
-    print(config, result.get_owned_games())
+def migrate_database(db_file_path: str):
+    backend = get_backend(f"sqlite:///{db_file_path}")
+    migrations = read_migrations("./src/migrations")
 
+    with backend.lock():
+        backend.apply_migrations(backend.to_apply(migrations))
+
+
+if __name__ == "__main__":
+    migrate_database("database.db")
+
+    config = load_config().config
+    result = SteamGamesApi(config["steam_api_key"], config["steam_id"])
+    print(config, result.get_owned_games())
