c95430f9c3ad9db3f17264f11be85a548f3552d5
dplocki <dawid.plocki@gmail.com>
2025-12-18 22:34:29 +0100

    feat(store): reduce the codebase for store

--- src/services/store.py
+++ src/services/store.py
@@ -1,38 +1,14 @@
-import sqlite3
-from yoyo import get_backend, read_migrations
+from typing import List
+from tinydb import TinyDB
 
 
 class Store:
     def __init__(self, db_file_path: str):
-        self.db_file_path = db_file_path
+        self.db = TinyDB(db_file_path)
+        self.events = self.db.table("events")
 
-    def migrate_database(self):
-        backend = get_backend(f"sqlite:///{self.db_file_path}")
-        migrations = read_migrations("./src/migrations")
+    def add_event(self, event):
+        self.events.insert({type: event.type})
 
-        with backend.lock():
-            backend.apply_migrations(backend.to_apply(migrations))
-
-    def get_stored_games(self):
-        with sqlite3.connect(self.db_file_path) as connection:
-            cursor = connection.cursor()
-            cursor.execute("SELECT id, name FROM games")
-            return cursor.fetchall()
-
-    def add_game(self, name: str):
-        with sqlite3.connect(self.db_file_path) as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "INSERT INTO games (name) VALUES (?)",
-                (name,),
-            )
-            connection.commit()
-
-    def update_game_required_time(self, game_id: int, required_time: int):
-        with sqlite3.connect(self.db_file_path) as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "UPDATE games SET howlongtobeat = ? WHERE id = ?",
-                (required_time, game_id),
-            )
-            connection.commit()
+    def get_all_events(self) -> List:
+        return self.events.all()
