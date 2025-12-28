95620e0cda9cc71ab8f772609282f7a114daff6d
dplocki <dawid.plocki@gmail.com>
2025-12-28 18:24:46 +0100

    feat(main): dynamic loading the commands

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import logging
+import importlib
+import inspect
+import pkgutil
+from typing import Generator
 from commands.refresh_games import RefreshGamesCommand
 from services.config import Configuration, load_config
 from services.games import Games
@@ -7,13 +11,29 @@ from services.store import Store
 from punq import Container
 
 
+def get_classes_from(package_name: str) -> Generator[type, None, None]:
+    pkg = importlib.import_module(package_name)
+    for _, modname, _ in pkgutil.iter_modules(pkg.__path__):
+        full_name = f"{package_name}.{modname}"
+        mod = importlib.import_module(full_name)
+        yield from (
+            obj
+            for _, obj in inspect.getmembers(mod, inspect.isclass)
+            if obj.__module__ == full_name
+        )
+
+
 def build_container(config: Configuration) -> Container:
     container = Container()
     container.register(Games)
-    container.register(SteamStore, factory=lambda: SteamStore(config.get_steam_api_key(), config.get_steam_id()))
+    container.register(
+        SteamStore,
+        factory=lambda: SteamStore(config.get_steam_api_key(), config.get_steam_id()),
+    )
     container.register(Store, factory=lambda: Store(config.get_database_path()))
 
-    container.register(RefreshGamesCommand)
+    for command_class in get_classes_from("commands"):
+        container.register(command_class)
 
     return container
 
