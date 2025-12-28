228b5561cd82f84df0d528d31df25c35e27f7483
dplocki <dawid.plocki@gmail.com>
2025-12-28 16:42:48 +0100

    chore(services): move config and howlongtobeat into services

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import logging
-from config import load_config
-from howlongtobeat import fetch_hltb_time
+from services.config import load_config
 from services.steam import SteamGamesApi
 from services.store import Store
-import punq
+from punq import Container
+
+
+def build_container():
+    container = Container()
+    container.register(SteamGamesApi)
+    container.register(Store, factory=lambda: Store(config.get_database_path()))
+
+    return container
+
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -13,35 +21,4 @@ if __name__ == "__main__":
     config = load_config()
     logging.info("Configuration loaded")
 
-    container = punq.Container()
-    container.register(SteamGamesApi)
-    container.register(Store, factory=lambda: Store(config.get_database_path()))
-
-    store = container.resolve(Store)
-    store.migrate_database()
-
-    stored_games = {game[1]: game for game in store.get_stored_games()}
-    stored_games_names = set(stored_games.keys())
-    logging.info("Games loaded from the database")
-
-    steamClient = SteamGamesApi(config.get_steam_api_key(), config.get_steam_id())
-    steam_games = {game["name"]: game for game in steamClient.get_owned_games()}
-    logging.info("Games loaded from the Steam")
-
-    steam_games_names = set(steam_games.keys())
-
-    for game_name in steam_games_names - stored_games_names:
-        logging.info("New game found: %s", game_name)
-        store.add_game(game_name)
-
-    for game_id, game_name, game_required_time in stored_games.values():
-        if game_required_time is not None:
-            continue
-
-        logging.info("Fetching HLTB time for: %s", game_name)
-        value = fetch_hltb_time(game_name)
-        if value is not None:
-            logging.info("Found HLTB time for %s: %d hours", game_name, value)
-            store.update_game_required_time(game_id, value)
-        else:
-            logging.info("HLTB time not found for: %s", game_name)
+    container = build_container()
