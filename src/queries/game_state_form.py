72cc8ca8ff88c103262c78af36b0c882b6f267d4
dplocki <dawid.plocki@gmail.com>
2026-01-10 14:40:48 +0100

    feat(queries): add game_state_form query

---
+++ src/queries/game_state_form.py
@@ -0,0 +1,16 @@
+from services.games import Games
+
+
+class GameStateFormQuery:
+    def __init__(self, games: Games):
+        self._games = games
+
+    def execute(self):
+        games = list(self._games.get_all_games())
+        games.sort(key=lambda g: g.name.lower())
+
+        print("Name,Platforms,Complete,Is not a game,Is different game")
+        for game in games:
+            print(
+                f'"{game.name}", {" ".join(game.available)}, {"Yes" if game.is_complete else "No"},,'
+            )
