83512b2f86a84bf99b48e1d841aea239023a27c8
dplocki <dawid.plocki@gmail.com>
2026-01-11 15:30:34 +0100

    feat(service store): the store is parsing the events

--- src/services/store.py
+++ src/services/store.py
@@ -1,16 +1,55 @@
-from typing import List
+from typing import Iterable, List
 from tinydb import TinyDB
 
-from models.event import AddGameEvent
+from models.event import (
+    AddEpicGameEvent,
+    AddGameEvent,
+    AddGogGameEvent,
+    AddSteamGameEvent,
+    DeleteGameEvent,
+    Event,
+    EventType,
+    MarkGameCompleteEvent,
+)
+from models.game_location import GameLocation
 
 
 class Store:
     def __init__(self, db_file_path: str):
         self.db = TinyDB(db_file_path)
-        self.events = self.db.table("events")
+        self.events = None
+        self.table_events = self.db.table("events")
 
     def add_event(self, event: AddGameEvent):
-        self.events.insert(event.__dict__)
+        self.table_events.insert(event.__dict__)
 
-    def get_all_events(self) -> List:
-        return self.events.all()
+    def get_all_events(self) -> Iterable[Event]:
+        if self.events is None:
+            self.events = list(map(parse_event, self.table_events.all()))
+
+        return self.events
+
+
+def parse_event(data: dict):
+    event_type: EventType = data.get("type", None)
+    del data["type"]
+
+    if event_type == EventType.ADD_GAME:
+        game_location = data.get("where_is")
+        del data["where_is"]
+
+        if game_location == GameLocation.STEAM:
+            return AddSteamGameEvent(**data)
+        elif game_location == GameLocation.GOG:
+            return AddGogGameEvent(**data)
+        elif game_location == GameLocation.EPIC:
+            return AddEpicGameEvent(**data)
+
+        raise ValueError(f"Unknown add game type: {event_type}")
+
+    elif event_type == EventType.DELETE_GAME:
+        return DeleteGameEvent(**data)
+    elif event_type == EventType.COMPLETED_GAME:
+        return MarkGameCompleteEvent(**data)
+
+    raise ValueError(f"Unknown event type: {event_type}")
