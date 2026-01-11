dc7dff060f28f2c08cbb171169b6750a009a0bac
dplocki <dawid.plocki@gmail.com>
2026-01-11 16:38:01 +0100

    test(refresh epic games): correct unit tests for new logic

--- tests/commands/test_refresh_epic_games.py
+++ tests/commands/test_refresh_epic_games.py
@@ -3,52 +3,49 @@ from unittest.mock import Mock
 from src.commands.refresh_epic_games import RefreshEpicGamesCommand
 from models.event import AddEpicGameEvent, DeleteGameEvent
 from models.game_location import GameLocation
-from tests.utils.data_providers import generate_game, generate_str
+from tests.utils.data_providers import generate_str
 
 
 def test_adds_new_epic_game_when_not_owned():
     new_game_name = generate_str()
 
-    games = Mock()
-    games.get_all_games.return_value = []
+    store = Mock()
+    store.get_all_events.return_value = []
 
-    cmd = RefreshEpicGamesCommand(games=games)
+    cmd = RefreshEpicGamesCommand(store)
     cmd.execute([new_game_name])
 
-    games.add_game.assert_called_once()
-    games.remove_game.assert_not_called()
+    store.add_event.assert_called_once()
+    store.remove_event.assert_not_called()
 
-    event = games.add_game.call_args[0][0]
+    event = store.add_event.call_args[0][0]
     assert isinstance(event, AddEpicGameEvent)
     assert event.name == new_game_name
 
 
 def test_skips_existing_epic_game():
     owned_game_name = generate_str()
-    owned_game = generate_game(name=owned_game_name, available={GameLocation.EPIC})
 
-    games = Mock()
-    games.get_all_games.return_value = [owned_game]
-    games.get_game.return_value = owned_game
+    store = Mock()
+    store.get_all_events.return_value = [AddEpicGameEvent(name=owned_game_name)]
 
-    cmd = RefreshEpicGamesCommand(games=games)
+    cmd = RefreshEpicGamesCommand(store)
     cmd.execute([owned_game_name])
 
-    games.add_game.assert_not_called()
-    games.remove_game.assert_not_called()
+    store.add_event.assert_not_called()
+    store.remove_event.assert_not_called()
 
 
 def test_removes_epic_game_not_own_anymore():
     owned_game_name = generate_str()
-    owned_game = generate_game(name=owned_game_name, available={GameLocation.EPIC})
 
-    games = Mock()
-    games.get_all_games.return_value = [owned_game]
+    store = Mock()
+    store.get_all_events.return_value = [AddEpicGameEvent(name=owned_game_name)]
 
-    cmd = RefreshEpicGamesCommand(games=games)
+    cmd = RefreshEpicGamesCommand(store)
     cmd.execute([])
 
-    games.remove_game.assert_called_once()
-    event = games.remove_game.call_args[0][0]
+    store.add_event.assert_called()
+    event = store.add_event.call_args[0][0]
     assert isinstance(event, DeleteGameEvent)
     assert event.name == owned_game_name and event.where_is == GameLocation.EPIC
