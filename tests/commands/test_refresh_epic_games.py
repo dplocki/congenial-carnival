a1c9002106d125eaf78c039871b27f8fc53616b6
dplocki <dawid.plocki@gmail.com>
2026-01-11 19:54:19 +0100

    refactor(unit tests): reduce constructor calls

--- tests/commands/test_refresh_epic_games.py
+++ tests/commands/test_refresh_epic_games.py
@@ -6,17 +6,20 @@ from models.game_location import GameLocation
 from tests.utils.data_providers import generate_str
 
 
+def build_add_epic_game_event(name: str) -> AddEpicGameEvent:
+    return AddEpicGameEvent(name=name)
+
+
 def test_adds_new_epic_game_when_not_owned():
     new_game_name = generate_str()
 
     store = Mock()
     store.get_all_events.return_value = []
 
-    cmd = RefreshEpicGamesCommand(store)
-    cmd.execute([new_game_name])
+    command = RefreshEpicGamesCommand(store)
+    command.execute([new_game_name])
 
     store.add_event.assert_called_once()
-    store.remove_event.assert_not_called()
 
     event = store.add_event.call_args[0][0]
     assert isinstance(event, AddEpicGameEvent)
@@ -27,23 +30,24 @@ def test_skips_existing_epic_game():
     owned_game_name = generate_str()
 
     store = Mock()
-    store.get_all_events.return_value = [AddEpicGameEvent(name=owned_game_name)]
+    store.get_all_events.return_value = [build_add_epic_game_event(owned_game_name)]
 
-    cmd = RefreshEpicGamesCommand(store)
-    cmd.execute([owned_game_name])
+    command = RefreshEpicGamesCommand(store)
+    command.execute([owned_game_name])
 
     store.add_event.assert_not_called()
-    store.remove_event.assert_not_called()
 
 
 def test_removes_epic_game_not_own_anymore():
     owned_game_name = generate_str()
 
     store = Mock()
-    store.get_all_events.return_value = [AddEpicGameEvent(name=owned_game_name)]
+    store.get_all_events.return_value = [
+        build_add_epic_game_event(owned_game_name),
+    ]
 
-    cmd = RefreshEpicGamesCommand(store)
-    cmd.execute([])
+    command = RefreshEpicGamesCommand(store)
+    command.execute([])
 
     store.add_event.assert_called()
     event = store.add_event.call_args[0][0]
