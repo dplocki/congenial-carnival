a00849d732e67d9b18d6f9476487f23e2216dcf2
dplocki <dawid.plocki@gmail.com>
2026-01-11 15:19:38 +0100

    feat(command refresh epic games): adjust the command for events as object

--- src/commands/refresh_epic_games.py
+++ src/commands/refresh_epic_games.py
@@ -1,28 +1,29 @@
 import logging
 from typing import Iterable
-from models.event import AddEpicGameEvent, DeleteGameEvent
+from models.event import AddEpicGameEvent, DeleteGameEvent, EventType
 from models.game_location import GameLocation
-from services.games import Games
+from services.store import Store
 
 
 logger = logging.getLogger(__name__)
 
 
 class RefreshEpicGamesCommand:
-    def __init__(self, games: Games):
-        self.games = games
+    def __init__(self, store: Store):
+        self.store = store
 
     def execute(self, games_titles: Iterable[str]) -> None:
         existing_titles = set(
-            game.name
-            for game in self.games.get_all_games()
-            if GameLocation.EPIC in game.available
+            event.name
+            for event in self.store.get_all_events()
+            if event.type == EventType.ADD_GAME
+            and event.where_is == GameLocation.EPIC
         )
 
         for title in set(games_titles) ^ existing_titles:
             if title in existing_titles:
                 logger.info(f"Removing game from Epic: {title}")
-                self.games.remove_game(DeleteGameEvent(title, GameLocation.EPIC))
+                self.store.add_event(DeleteGameEvent(title, GameLocation.EPIC))
             else:
                 logger.info(f"Adding new game on Epic: {title}")
-                self.games.add_game(AddEpicGameEvent(title))
+                self.store.add_event(AddEpicGameEvent(title))
