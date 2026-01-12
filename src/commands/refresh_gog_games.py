e1fd2df73fa1366f95ae6313a9386be8c68fa06a
dplocki <dawid.plocki@gmail.com>
2026-01-12 21:08:54 +0100

    fix(refresh gog games): adjust code for using the store instead of game service

--- src/commands/refresh_gog_games.py
+++ src/commands/refresh_gog_games.py
@@ -1,45 +1,53 @@
 import logging
 from operator import attrgetter
-from typing import Dict, Iterable
-from models.event import AddGogGameEvent, DeleteGogGameEvent, MarkGameCompleteEvent
+from typing import Iterable, TypedDict
+from models.event import (
+    AddGogGameEvent,
+    DeleteGogGameEvent,
+    EventType,
+    MarkGameCompleteEvent,
+)
 from models.game_location import GameLocation
-from services.config import Configuration
-from services.games import Games
+from services.store import Store
 
 
 logger = logging.getLogger(__name__)
 get_name = attrgetter("name")
+COMPLETED_TAG = "COMPLETED"
+
+
+class GameData(TypedDict):
+    title: str
+    id: int
+    tags: Iterable[str]
 
 
 class RefreshGogGamesCommand:
-    def __init__(self, config: Configuration, games: Games):
-        self.config = config
-        self.games = games
+    def __init__(self, store: Store):
+        self.store: Store = store
 
-    def execute(self, games_data: Iterable[Dict]) -> None:
-        already_own_games = set(
-            map(
-                get_name,
-                filter(
-                    lambda g: GameLocation.GOG in g.available,
-                    self.games.get_all_games(),
-                ),
-            )
-        )
+    def execute(self, games_data: Iterable[GameData]) -> None:
+        already_own_gog_games = set()
+        completed_games = set()
+
+        for event in self.store.get_all_events():
+            if event.type == EventType.ADD_GAME and event.where_is == GameLocation.GOG:
+                already_own_gog_games.add(event.name)
+            elif event.type == EventType.COMPLETED_GAME:
+                completed_games.add(event.name)
 
         for game_datum in games_data:
             title = game_datum["title"].strip()
-            if title not in already_own_games:
+            if title not in already_own_gog_games:
                 logger.info(f"Adding new game on Gog: {title}")
-                self.games.add_game(AddGogGameEvent(title, game_datum["id"]))
+                self.store.add_event(AddGogGameEvent(title, game_datum["id"]))
             else:
-                already_own_games.remove(title)
+                already_own_gog_games.remove(title)
 
-            game = self.games.get_game(title)
-            if not game.is_complete and "COMPLETED" in game_datum["tags"]:
+            if title not in completed_games and COMPLETED_TAG in game_datum["tags"]:
                 logger.info(f"Game complete: {title} (Gog)")
-                self.games.change_game_state(MarkGameCompleteEvent(title))
+                self.store.add_event(MarkGameCompleteEvent(title))
 
-        for game_name in already_own_games:
+        for game_name in already_own_gog_games:
             logger.info(f"Removing game from Gog: {game_name}")
-            self.games.remove_game(DeleteGogGameEvent(game_name))
+            self.store.add_event(DeleteGogGameEvent(game_name))
--- tests/commands/test_refresh_gog_games.py
+++ tests/commands/test_refresh_gog_games.py
@@ -1,8 +1,7 @@
 from unittest.mock import Mock
-from src.commands.refresh_gog_games import RefreshGogGamesCommand
+from src.commands.refresh_gog_games import GameData, RefreshGogGamesCommand
 from models.event import AddGogGameEvent, MarkGameCompleteEvent
-from models.game_location import GameLocation
-from tests.utils.data_providers import generate_game, generate_int, generate_str
+from tests.utils.data_providers import generate_int, generate_str
 
 
 def build_add_gog_game_event(title: str, gog_id: int = None) -> dict:
@@ -15,7 +14,7 @@ def build_mark_game_complete_event(title: str) -> dict:
 
 def build_gog_game_entity(
     title: str, gog_id: int = None, is_complete: bool = False
-) -> dict:
+) -> GameData:
     return {
         "title": title,
         "id": gog_id or generate_int(),
@@ -46,7 +45,7 @@ def test_skips_existing_gog_game():
     store.get_all_events.return_value = [build_add_gog_game_event(owned_game_name)]
 
     cmd = RefreshGogGamesCommand(store)
-    cmd.execute([build_gog_game_entity(owned_game_name, is_complete=True)])
+    cmd.execute([build_gog_game_entity(owned_game_name)])
 
     store.add_event.assert_not_called()
 
@@ -55,13 +54,15 @@ def test_marks_existing_game_complete():
     owned_game_name = generate_str()
 
     store = Mock()
-    store.get_all_events.return_value = [build_add_gog_game_event(owned_game_name)]
+    store.get_all_events.return_value = [
+        build_add_gog_game_event(owned_game_name),
+    ]
 
     cmd = RefreshGogGamesCommand(store)
     cmd.execute([build_gog_game_entity(owned_game_name, is_complete=True)])
 
-    store.change_game_state.assert_called_once()
-    evt = store.change_game_state.call_args[0][0]
+    store.add_event.assert_called_once()
+    evt = store.add_event.call_args[0][0]
     assert isinstance(evt, MarkGameCompleteEvent) and evt.name == owned_game_name
 
 
@@ -74,7 +75,7 @@ def test_does_not_mark_if_already_complete():
         build_mark_game_complete_event(owned_game_name),
     ]
 
-    cmd = RefreshGogGamesCommand(config=Mock(), games=store)
+    cmd = RefreshGogGamesCommand(store)
     cmd.execute([build_gog_game_entity(owned_game_name, is_complete=True)])
 
     store.add_event.assert_not_called()
