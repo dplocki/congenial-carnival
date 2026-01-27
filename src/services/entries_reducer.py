ab94e17a37f3d341b97bece1f409cab1c28cfd0f
dplocki <dawid.plocki@gmail.com>
2026-01-27 22:34:36 +0100

    refactor(entries reducer): adjust the name of the field

--- src/services/entries_reducer.py
+++ src/services/entries_reducer.py
@@ -9,16 +9,16 @@ class EntriesReducer:
 
     def __init__(self, store: Store):
         self.store = store
-        self.games: Dict[str, Entry] = None
+        self.entries: Dict[str, Entry] = None
 
     def get_all_entries(self) -> Iterable[Entry]:
-        if self.games is None:
+        if self.entries is None:
             self.__build_games_directory()
 
-        return self.games.values()
+        return self.entries.values()
 
     def __build_games_directory(self) -> None:
-        self.games = {}
+        self.entries = {}
 
         for event in self.store.get_all_events():
             self.__reduce_event(event)
@@ -27,42 +27,42 @@ class EntriesReducer:
         if event["type"] == EventType.ADD_GAME:
             game_name = event["name"]
 
-            if game_name not in self.games:
-                self.games[game_name] = Entry(event["name"])
+            if game_name not in self.entries:
+                self.entries[game_name] = Entry(event["name"])
 
-            self.games[game_name].available.add(event["where_is"])
+            self.entries[game_name].available.add(event["where_is"])
 
         elif event["type"] == EventType.DELETE_GAME:
             game_name = event["name"]
             location = event["where_is"]
 
-            if game_name not in self.games:
+            if game_name not in self.entries:
                 return
 
-            self.games[game_name].available.remove(location)
-            if not self.games[game_name].available:
-                del self.games[game_name]
+            self.entries[game_name].available.remove(location)
+            if not self.entries[game_name].available:
+                del self.entries[game_name]
 
         elif event["type"] == EventType.COMPLETED_GAME:
             game_name = event["name"]
-            game = self.games[game_name]
-            self.games[game_name] = Entry(game.name, game.available, is_complete=True)
+            game = self.entries[game_name]
+            self.entries[game_name] = Entry(game.name, game.available, is_complete=True)
 
         elif event["type"] == EventType.RENAME_GAME:
             old_game_name = event["old_name"]
             new_game_name = event["new_name"]
 
-            new_game = self.games.get(new_game_name, Entry(new_game_name))
+            new_game = self.entries.get(new_game_name, Entry(new_game_name))
 
-            old_game = self.games[old_game_name]
+            old_game = self.entries[old_game_name]
             aliases = old_game.aliases | new_game.aliases
             aliases.add(old_game_name)
 
             available = old_game.available | new_game.available
             is_complete = old_game.is_complete or new_game.is_complete
 
-            self.games[new_game_name] = Entry(
+            self.entries[new_game_name] = Entry(
                 new_game_name, available, aliases, is_complete
             )
 
-            del self.games[old_game_name]
+            del self.entries[old_game_name]
