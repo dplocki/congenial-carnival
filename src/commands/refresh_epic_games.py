294743e2393bac0043ab954964d1ac8eba4f6ef9
dplocki <dawid.plocki@gmail.com>
2026-01-27 23:30:42 +0100

    feat(epic refresh games): readapt the command to use the entries service

--- src/commands/refresh_epic_games.py
+++ src/commands/refresh_epic_games.py
@@ -3,27 +3,25 @@ from models.event import Event
 from typing import Generator, Iterable
 from models.event import AddEpicGameEvent, DeleteGameEvent, EventType
 from models.game_location import GameLocation
-from services.store import Store
+from services.entries_reducer import EntriesReducer
 
 
 logger = logging.getLogger(__name__)
 
 
 class RefreshEpicGamesCommand:
-    def __init__(self, store: Store):
-        self.store = store
+    def __init__(self, entries_reducer: EntriesReducer):
+        self.store = entries_reducer
 
     def execute(self, games_titles: Iterable[str]) -> Generator[Event, None, None]:
-        existing_titles = set(
-            event.name
-            for event in self.store.get_all_events()
-            if event.type == EventType.ADD_GAME and event.where_is == GameLocation.EPIC
-        )
+        existing_titles = set()
+        for entry in self.store.get_all_entries():
+            if GameLocation.EPIC not in entry.available:
+                continue
+
+            existing_titles.add(entry.name)
+            existing_titles.update(entry.aliases)
 
         for title in set(games_titles) ^ existing_titles:
-            if title in existing_titles:
-                logger.info(f"Removing game from Epic: {title}")
-                yield DeleteGameEvent(title, GameLocation.EPIC)
-            else:
-                logger.info(f"Adding new game on Epic: {title}")
-                yield AddEpicGameEvent(title)
+            logger.info(f"Adding new game on Epic: {title}")
+            yield AddEpicGameEvent(title)
--- tests/commands/test_refresh_epic_games.py
+++ tests/commands/test_refresh_epic_games.py
@@ -1,55 +1,46 @@
 from typing import Iterable
 from unittest.mock import Mock
 
+from models.entry import Entry
+from services.entries_reducer import EntriesReducer
 from src.commands.refresh_epic_games import RefreshEpicGamesCommand
 from models.event import AddEpicGameEvent, DeleteGameEvent
 from models.game_location import GameLocation
 from models.event import Event
-from tests.utils.data_providers import generate_str
+from tests.utils.data_providers import generate_entry, generate_str
 
 
-def build_add_epic_game_event(name: str) -> AddEpicGameEvent:
-    return AddEpicGameEvent(name=name)
-
-
-def execute_command(store: Mock, games_titles: Iterable[str]) -> Iterable[Event]:
-    command = RefreshEpicGamesCommand(store)
+def execute_command(
+    entries_reducer: Mock, games_titles: Iterable[str]
+) -> Iterable[Event]:
+    command = RefreshEpicGamesCommand(entries_reducer)
     return list(command.execute(games_titles))
+
+
+def build_entry_reducer(*args: Entry) -> EntriesReducer:
+    entries_reducer = Mock()
+    entries_reducer.get_all_entries.return_value = args
+    return entries_reducer
+
+
+def build_epic_entry(**kwargs) -> Entry:
+    kwargs["available"] = [GameLocation.EPIC]
+    return generate_entry(**kwargs)
 
 
 def test_adds_new_epic_game_when_not_owned():
     new_game_name = generate_str()
+    entries_reducer = build_entry_reducer()
 
-    store = Mock()
-    store.get_all_events.return_value = []
+    event = execute_command(entries_reducer, [new_game_name])[0]
 
-    event = execute_command(store, [new_game_name])[0]
-
-    assert isinstance(event, AddEpicGameEvent)
     assert event.name == new_game_name
 
 
 def test_skips_existing_epic_game():
     owned_game_name = generate_str()
+    entries_reducer = build_entry_reducer(generate_entry(name=owned_game_name))
 
-    store = Mock()
-    store.get_all_events.return_value = [build_add_epic_game_event(owned_game_name)]
+    result = execute_command(entries_reducer, [owned_game_name])
 
-    command = RefreshEpicGamesCommand(store)
-    command.execute([owned_game_name])
-
-    store.add_event.assert_not_called()
-
-
-def test_removes_epic_game_not_own_anymore():
-    owned_game_name = generate_str()
-
-    store = Mock()
-    store.get_all_events.return_value = [
-        build_add_epic_game_event(owned_game_name),
-    ]
-
-    event = execute_command(store, [])[0]
-
-    assert isinstance(event, DeleteGameEvent)
-    assert event.name == owned_game_name and event.where_is == GameLocation.EPIC
+    return len(result) == 0
