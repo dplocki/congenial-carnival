33abea6b674c614f7b2b40a1e5ccf13186467a06
dplocki <dawid.plocki@gmail.com>
2026-02-05 22:30:51 +0100

    tests(to_csv presenter): remove hardcoded strings from unit tests

--- tests/presenters/test_to_csv.py
+++ tests/presenters/test_to_csv.py
@@ -1,29 +1,38 @@
 import io
+from typing import Dict, Generator, List, Union
 from presenters.to_csv import ToCsvPresenter
 from tests.utils.data_providers import generate_int, generate_str
 
 
-def build_presenter(data):
+def build_presenter(data: Generator[Dict, None, None]) -> str:
     presenter = ToCsvPresenter()
     output = io.StringIO()
     presenter.present(data, output)
     return output.getvalue()
 
 
+def build_input_data(
+    headers: List[str], rows: List[List[Union[int, str]]]
+) -> Generator[Dict, None, None]:
+    for row in rows:
+        yield {header: value for header, value in zip(headers, row)}
+
+
 def test_presenter_should_generate_csv_headers():
-    data = [{"id": generate_int(), "name": generate_str(), "category": generate_str()}]
+    headers = [generate_str() for _ in range(3)]
+    data = build_input_data(headers, [[generate_int(), generate_str(), generate_str()]])
 
     result = build_presenter(data)
 
-    assert result.splitlines()[0] == "id,name,category"
+    assert result.splitlines()[0].split(",") == headers
 
 
 def test_presenter_should_generate_csv_rows():
-    data = [
-        {"id": 1, "name": "Game A", "category": "Action"},
-        {"id": 2, "name": "Game B", "category": "Adventure"},
-    ]
+    headers = [generate_str() for _ in range(3)]
+    rows = [(generate_int(), generate_str(), generate_str()) for _ in range(5)]
 
+    data = build_input_data(headers, rows)
     result = build_presenter(data)
 
-    assert result.splitlines() == ["id,name,category", "1,Game A,Action", "2,Game B,Adventure"]
+    for line, row in zip(result.splitlines()[1:], rows):
+        assert line.split(",") == list(map(str, row))
