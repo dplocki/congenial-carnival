c3fd9d5ef0f09e58e84876a25095ca1dd7405995
dplocki <dawid.plocki@gmail.com>
2026-02-08 22:23:59 +0100

    feat(game state form query): add sorting the form

--- src/queries/game_state_form.py
+++ src/queries/game_state_form.py
@@ -15,14 +15,13 @@ class GameStateFormQuery:
         self.entries_reducer = entries_reducer
 
     def execute(self) -> Generator[GameStateFormData, None, None]:
-        for entry in self.entries_reducer.get_all_entries():
-            if not entry.is_game:
-                continue
+        only_games = filter(lambda entry: entry.is_game, self.entries_reducer.get_all_entries())
 
+        for entry in sorted(only_games, key=lambda entry: entry.name):
             yield GameStateFormData(
                 name=entry.name,
                 platforms=" ".join(entry.available),
                 is_complete=entry.is_complete,
-                is_not_a_game=False,
+                is_not_a_game=not entry.is_game,
                 is_different_game=False,
             )
